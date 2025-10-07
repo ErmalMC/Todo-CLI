9a2bac9e1b5028fdacb51ee07977b6724d6b620f
Ermal Baki <59308440+ErmalMC@users.noreply.github.com>
2025-10-07 21:36:01 +0200

    Loop And Introduction
    Added a main loop so that the program continues to work with multiple inputs

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -1,13 +1,38 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         TaskManager manager = new TaskManager();
 
         System.out.println("Welcome to your To Do Program");
+        System.out.println("Available Commands:");
+        System.out.println("1. add <description>");
+        System.out.println("2. list");
+        System.out.println("3. done <task number>");
+        System.out.println("4. remove <task number>");
+        System.out.println("5. exit");
 
         while (true) {
+            System.out.println("\n");
+            String input = scanner.nextLine();
+            String[] parts = input.split(" ",2);
+            String command = parts[0].toLowerCase();
+
+            switch(command){
+                case "add":
+                    break;
+                case "list":
+                    break;
+                case "done":
+                    break;
+                case "remove":
+                    break;
+                case "exit":
+                    break;
+                default:
+                    System.out.println("Invalid Command");
+            }
 
         }
 
